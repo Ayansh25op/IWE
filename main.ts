4d2887c0916a109823ac1911afddacd13352039e
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-09-16 15:34:16 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+let isSwitched = false
+let force = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString(" Triggering Self Destruct In 10 Seconds")
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.Butterfly)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.Butterfly)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Calling Suit")
+})
+input.onButtonPressed(Button.B, function () {
+    if (isSwitched) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+basic.forever(function () {
+    force = Math.abs(input.magneticForce(Dimension.Strength))
+    isSwitched = force > 100
+})
