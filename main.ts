5fd7382aeacb1ed4d0af2837f08023cf5d0e0581
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-09-24 09:17:53 +0530

    Removed Micro:Bit V1 Support.Micro:bit V2 Support Added.Added Sounds,Modified Startup,Redirected Self Distruct To On Logo Long Pressed,Added Emergency Protocol To A+B Added Shield To Logo Pressed

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("SELF DESTRUCT IN 10 SECONDS")
+})
 input.onButtonPressed(Button.A, function () {
+    soundExpression.spring.playUntilDone()
     for (let index = 0; index < 5; index++) {
         basic.showIcon(IconNames.Butterfly)
         basic.showLeds(`
@@ -8,23 +12,30 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             . . . . .
             `)
+        basic.showString("CALLING SUIT")
     }
-    basic.showString("CALLING SUIT")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("SELF DESTRUCT IN 10 SECONDS")
+    basic.showString("Shield Activated")
+    soundExpression.twinkle.playUntilDone()
 })
 input.onButtonPressed(Button.B, function () {
+    soundExpression.slide.playUntilDone()
     if (isSwitched) {
         basic.showIcon(IconNames.Yes)
     } else {
         basic.showIcon(IconNames.No)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    soundExpression.soaring.playUntilDone()
+    basic.showString("Initiating Emergency Protocol ")
+})
 let force = 0
 let isSwitched = false
-basic.showString("POWER ON!")
+soundExpression.giggle.playUntilDone()
 basic.showIcon(IconNames.Chessboard)
+basic.pause(1000)
 basic.clearScreen()
 basic.forever(function () {
     force = Math.abs(input.magneticForce(Dimension.Strength))
