215ba77cb44e50451964f929803b45d83d443b64
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-10-12 09:27:22 +0530

    If The Other microbit is in range then it shown success and if other microbit is not in range it shown Falied

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 enum RadioMessage {
     message1 = 49434
 }
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        RadioConnection = 1
+    }
+})
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     radio.sendNumber(35)
-    basic.showString("Success!")
+    basic.pause(500)
+    if (RadioConnection == 1) {
+        basic.showString("Success!")
+    } else {
+        basic.showString("Failed")
+    }
+    RadioConnection = 0
 })
 input.onButtonPressed(Button.A, function () {
     soundExpression.spring.playUntilDone()
@@ -37,7 +48,9 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 let force = 0
 let isSwitched = false
+let RadioConnection = 0
 radio.setGroup(35)
+RadioConnection = 0
 soundExpression.giggle.playUntilDone()
 basic.showIcon(IconNames.Chessboard)
 basic.pause(1000)
